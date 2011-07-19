0c0d4171c421dd3180e039e9c465434fcb53f5e0
Tom Randolph <websites@thomasrandolph.info>
2011-07-18 20:15:31 -0500

    ~ added default login admin@example.com:password

--- _project_files/afterthought.sql
+++ _project_files/afterthought.sql
@@ -2,6 +2,8 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
+CREATE SCHEMA IF NOT EXISTS `afterthought` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+USE `afterthought` ;
 
 -- -----------------------------------------------------
 -- Table `afterthought`.`users`
@@ -56,3 +58,30 @@ CREATE  TABLE IF NOT EXISTS `afterthought`.`authentication` (
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `afterthought`.`users`
+-- -----------------------------------------------------
+SET AUTOCOMMIT=0;
+USE `afterthought`;
+INSERT INTO `afterthought`.`users` (`userid`, `fname`, `lname`, `email`, `nickname`) VALUES (1, 'John', 'Doe', 'admin@example.com', 'John');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `afterthought`.`roles`
+-- -----------------------------------------------------
+SET AUTOCOMMIT=0;
+USE `afterthought`;
+INSERT INTO `afterthought`.`roles` (`roleid`, `description`, `name`) VALUES (1, 'Site Administrator, full privileges for all access.', 'Administrator');
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `afterthought`.`authentication`
+-- -----------------------------------------------------
+SET AUTOCOMMIT=0;
+USE `afterthought`;
+INSERT INTO `afterthought`.`authentication` (`authenticationid`, `identity`, `password`, `salt`, `roleid`, `userid`, `resetPassword`, `disabled`) VALUES (NULL, 'admin@example.com', '8462e4dde835a9cbcaf906a52cd1731e845c1fb8aaed0fffcd781f8004cde292d3327e37926fe2d5a1d2e3b5420f9971c16cb03ddb8b89e33d4783981d4ccf6d', 'OYkjNtmVVrdwolePdHwB73MgfldxDVnd65laqCI1fU96OfBRWMrfm2gKTyhtIiP2', 1, 1, 0, 0);
+
+COMMIT;
