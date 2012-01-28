975ff3d01bbfc0708f7e1f334d6eeef127300701
Tom Randolph <websites@thomasrandolph.info>
2012-01-27 20:49:20 -0600

    ~ header interactivity

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,37 @@
-$( '#logo,#title' ).click(function(){
+$( '#title' ).click(function(){
 	go_here('index.php');
 });
 
-log.binfo( 'You can log in with "admin@example.com" and "password"' );
+$( '#head_logout' ).hover(
+	function(){
+		$(this).children('img').attr('src', 'images/icons/door-open-out.png');
+	},
+	function(){
+		$(this).children('img').attr('src', 'images/icons/door--arrow.png');
+	}
+);
+
+//element default left
+Tipped.create('.tipped-left', {
+	hideOn: [
+				{ element: 'self', event: 'mouseleave' },
+				{ element: 'tooltip', event: 'mouseenter' }
+			],
+	hook: 'leftmiddle'
+});
+
+//element fixed below
+$('.fixedElementTipped-below').each(
+	function()
+	{
+		var element = $(this).attr('data-tippedID');
+		Tipped.create(
+			this,
+			$('#' + element).clone(true, true).removeAttr('id')[0],
+			{
+				hook: { target:  'bottomleft', tooltip: 'topleft' },
+				skin: 'light'
+			}
+		);
+	}
+);
