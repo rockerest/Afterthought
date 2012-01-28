1d64b247ca3412cec932d11491965136b5eeba43
Tom Randolph <websites@thomasrandolph.info>
2012-01-27 20:45:15 -0600

    + added users page (user management)

---
+++ users.php
@@ -0,0 +1,97 @@
+<?php
+	set_include_path('backbone:components:content:model:render:scripts:styles:images');
+	
+	require_once('Page.php');
+	require_once('Template.php');
+	require_once('User.php');
+	
+	$user = User::getByID($_SESSION['userid']);
+	
+	secure(2); // only allow admins or staff (roleid <= 2)
+
+	$page = new Page("Users :: Afterthought");
+	$tmpl = new Template();
+	
+	$page->run();
+	
+	$tmpl->code = isset( $_GET['code'] ) ? $_GET['code'] : -1;
+	$tmpl->users = User::toArray(User::getAll());
+	
+	switch( $tmpl->code )
+	{
+		case 0:
+				// filler error
+				$tmpl->type = "error";
+				$tmpl->message = "I'm sorry Dave, I can't let you do that.";
+				break;
+		case 3:
+				// Employee was deleted successfully
+				$tmpl->type = "okay";
+				$tmpl->message = "The employee was deleted successfully.";
+				break;
+		case 4:
+				// Employee was deleted successfully, cascade delete failed
+				$tmpl->type = "error";
+				$tmpl->message = "The employee was deleted, but associated data could not be removed.  Please contact an administrator.<br />" . $tmpl->errorcode;
+				break;
+		case 5:
+				// Employee was disabled successfully
+				$tmpl->type = "okay";
+				$tmpl->message = "The employee login was disabled successfully.";
+				break;
+		case 6:
+				// Employee was enabled successfully
+				$tmpl->type = "okay";
+				$tmpl->message = "The employee login was enabled successfully.";
+				break;
+		case 7:
+				// Employee disable failed
+				$tmpl->type = "error";
+				$tmpl->message = "The employee login could not be disabled.";
+				break;
+		case 8:
+				// Employee enable failed
+				$tmpl->type = "error";
+				$tmpl->message = "The employee login could not be enabled.";
+				break;
+		case 9:
+				// Force password change succeeded
+				$tmpl->type = "okay";
+				$tmpl->message = "The employee will be prompted to change their password at next login.";
+				break;
+		case 10:
+				// accept current password succeeded
+				$tmpl->type = "okay";
+				$tmpl->message = "The employee will not be prompted to change their password.";
+				break;
+		case 11:
+				// Force password change failed
+				$tmpl->type = "error";
+				$tmpl->message = "Attempting to prompt for the employee to change their password at next login failed.";
+				break;
+		case 12:
+				// accept current password failed
+				$tmpl->type = "error";
+				$tmpl->message = "Attempting to remove the prompt for the employee to change their password at next login failed.";
+				break;
+		default:
+				break;
+	}
+	
+	$html = $tmpl->build('users.html');
+	$css = $tmpl->build('users.css');
+	$js = $tmpl->build('users.js');
+	
+	$appContent = array(
+						'html'	=>	$html,
+						'css'	=>	array(	'code' => $css,
+											'link' => 'users'
+											),
+						'js' => array(	'code' => $js,
+										'link' => 'users'
+										)
+						);
+
+	print $page->build($appContent);
+
+?>
