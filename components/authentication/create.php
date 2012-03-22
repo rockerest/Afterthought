9691a361865f6b1b8e85e4b75603f0cdc6036ae9
Tom Randolph <websites@thomasrandolph.info>
2012-03-21 21:51:42 -0500

    + added email templates ~ switched account emails to templated mail

--- components/authentication/create.php
+++ components/authentication/create.php
@@ -5,6 +5,7 @@
 	require_once('User.php');
 	require_once('Session.php');
 	require_once('Quick_Login.php');
+	require_once('Mail.php');
 	setSession(0, '/');
 	
 	$password = isset($_POST['password']) ? $_POST['password'] : null;
@@ -22,46 +23,24 @@
 			$user = User::add($data['fname'],$data['lname'],$data['email'],$password,3);
 			if( $user )
 			{
-				$user->resetPassword = 0;
-				$user->disabled = 1;
-				$user->save();
+				$auth = $user->authentication;
+				$auth->resetPassword = 0;
+				$auth->disabled = 1;
+				$auth->save();
 				
 				//create login hash
-				$hash = hash('whirlpool', $user->authentication->password . time() . $user->authentication->salt);
+				$hash = hash('whirlpool', $user->authentication->identity . time() . (time() / 64));
 				if( !Quick_Login::add($hash, $user->userid, time() + 3600, 0) )
 				{
 					// die
 				}
 				
-				//send email
-				$body = <<<HEREDOCend
-<html>
-<body>
-	<div style="width: 600px; border: 2px solid #E9EBF6; margin: auto; font-size: 16px; color: #555555;">
-		<h1 style="margin: 0; padding: 8px; background-color: #E9EBF6; text-align: center;">
-			Hello, {$user->fname}!
-		</h1>
-		<div style="overflow: hidden; padding: 8px; padding-top: 0; background-color: #F5F6FB;">
-			<p>You are receiving this email because you (or someone pretending to be you!) has signed up for a new account on the Afterthought System.</p>
-			<p>If you would like to verify this email account (and you must in order to use the system), please <a href="http://afterthought.thomasrandolph.info/components/authentication/verify.php?code={$hash}">click this link</a>.</p>
-			<p>If you don't know what this is about, or you don't want the account, simply do nothing.</p>
-			<p>The quick login link above is a one-time access pass to your account.  Please use the link to verify your email address and complete your account signup.</p>
-			<br />
-			<p>Thanks!</p>
-			<p>-Afterthought</p>
-		</div>
-	</div>
-</body>
-</html>
-HEREDOCend;
-				$subject = "Afterthought System Email Verification";
-				$to = $user->contact->email;
-				$headers =	'From: no-reply-automator@afterthought.thomasrandolph.info' . "\r\n" .
-							'Content-Type: text/html; charset=iso-8859-1' . "\r\n" . 
-							'X-Mailer: PHP/' . phpversion() . "\r\n" . 
-							'MIME-Version: 1.0' . "\r\n";
+				//load email template
+				ob_start();
+				include('templates/account_create.html');
+				$body = ob_get_clean();
 				
-				if( mail($to, $subject, $body, $headers) )
+				if( Mail::sendMail($user->contact->email, 'no-reply-automator@afterthought.thomasrandolph.info', "Afterthought System Database Email Verification", $body) )
 				{
 					//redirect to login			
 					throw new RedirectBrowserException("/index.php?code=6");
--- components/authentication/recover.php
+++ components/authentication/recover.php
@@ -4,6 +4,7 @@
 	require_once('User.php');
 	require_once('Session.php');
 	require_once('Quick_Login.php');
+	require_once('Mail.php');
 	setSession(0, '/');
 	
 	$email = isset($_GET['email']) ? $_GET['email'] : null;
@@ -25,37 +26,12 @@
 					// die
 				}
 				
-				//send email
-				$body = <<<HEREDOCend
-<html>
-<body>
-	<div style="width: 600px; border: 2px solid #E9EBF6; margin: auto; font-size: 16px; color: #555555;">
-		<h1 style="margin: 0; padding: 8px; background-color: #E9EBF6; text-align: center;">
-			Hello, {$user->fname}!
-		</h1>
-		<div style="overflow: hidden; padding: 8px; padding-top: 0; background-color: #F5F6FB;">
-			<p>You are receiving this email because you (or someone pretending to be you!) requested that your account be recovered.</p>
-			<p>It's possible you forgot your password, or you forgot you even had an account.</p>
-			<p>That's okay.</p>
-			<p>To reset your password and log in, please <a href="http://afterthought.thomasrandolph.info/components/authentication/verify.php?code={$hash}">click this link</a>.</p>
-			<p>If you don't know what this is about, or you don't want the account, simply do nothing.</p>
-			<p>The quick login link above is a one-time access pass to your account.  Please use it to reset your password and log in this time.</p>
-			<br />
-			<p>Thanks!</p>
-			<p>-Afterthought</p>
-		</div>
-	</div>
-</body>
-</html>
-HEREDOCend;
-				$subject = "Aferthought System Account Recovery";
-				$to = $user->contact->email;
-				$headers =	'From: no-reply-automator@afterthought.thomasrandolph.info' . "\r\n" .
-							'Content-Type: text/html; charset=iso-8859-1' . "\r\n" . 
-							'X-Mailer: PHP/' . phpversion() . "\r\n" . 
-							'MIME-Version: 1.0' . "\r\n";
+				//load email template
+				ob_start();
+				include('templates/account_recover.html');
+				$body = ob_get_clean();
 				
-				if( mail($to, $subject, $body, $headers) )
+				if( Mail::sendMail($user->contact->email, 'no-reply-automator@afterthought.thomasrandolph.info', "Afterthought System Account Recovery", $body) )
 				{
 					//redirect to login			
 					throw new RedirectBrowserException("/index.php?code=6");
